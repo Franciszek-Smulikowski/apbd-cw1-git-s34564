ddb876c4782b6dfc54759e040e27d828e0e92d2a
Franciszek-Smulikowski <franciszek.smulikowski@gmail.com>
2026-03-31 22:02:34 +0200

    Add CalculateMax method

--- ConsoleAppS34564/StatisticsHelper.cs
+++ ConsoleAppS34564/StatisticsHelper.cs
@@ -29,5 +29,23 @@ namespace ConsoleAppS34564
             int sum = CalculateSum(values);
             return (double)sum / values.Length;
         }
+
+        public static int CalculateMax(int[] values)
+        {
+            if (values == null || values.Length == 0)
+            {
+                throw new ArgumentException("Tablica nie może być pusta ani null.");
+            }
+
+            int max = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > max)
+                {
+                    max = values[i];
+                }
+            }
+            return max;
+        }
     }
 }
